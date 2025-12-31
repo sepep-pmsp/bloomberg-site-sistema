9ad3fc603366790bae266f55aff12fcdf50fda31
Ana Carolina <carol00_10@hotmail.com>
2025-12-31 16:57:57 -0300

    Add EJS view engine and static assets setup
    Configured EJS as the view engine using ejs-mate, set up views and static assets directories, and added cookie-parser and urlencoded middleware. Updated routes import to use './routes/main'.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const engine = require('ejs-mate');
+const path = require('path');
 
 const connectDB = require('./config/db/database');
-const routes = require('./routes');
+const routes = require('./routes/main'); 
 
 const app = express();
+const cookieParser = require('cookie-parser');
 connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurações de View Engine (EJS e Pasta Public)
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'modules', 'admin', 'views'));
+app.use(express.static(path.join(__dirname, 'modules', 'admin', 'public')));
+
+// Use as rotas DEPOIS de configurar as views e estáticos
 app.use(routes);
 
 const PORT = process.env.PORT || 3001;
