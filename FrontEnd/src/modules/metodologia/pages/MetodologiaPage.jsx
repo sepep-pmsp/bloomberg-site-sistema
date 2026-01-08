dc717a531434fad92feb7eb0719865a14ad32e23
Ana Carolina <121954394+Ana-CAS-Machado@users.noreply.github.com>
2026-01-08 12:01:19 -0300

    Page metodologia (#7)
    * Refactor navigation to use react-router's navigate
    
    Updated Navbar to use NavLink for active tab styling and navigation. Replaced window.location.href with useNavigate in Login and Register pages for programmatic navigation after login and registration, improving SPA routing and user experience.
    
    * add Bg-Images
    
    * Add metodologia module and update styles
    
    Introduced the metodologia module with new components, sections, and styles. Added AppLayout and a banner image. Updated Login page and global styles to integrate the new module and layout.

---
+++ FrontEnd/src/modules/metodologia/pages/MetodologiaPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../sections/Banner'
+import Reducoes from '../sections/Reducoes'
+import Cenarios from '../sections/Cenarios'
+
+export default function MetodologiaPage() {
+  return (
+    <>
+        <Banner />
+        <Reducoes/>
+        <Cenarios/>
+    </>
+  )
+}
